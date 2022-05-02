f85735fd75883dff3de4066d1840317337f0e86a
Prashan <prashan@epitcore.com>
2022-05-03 03:02:11 +0530

    Add project files.

---
+++ Scripts/function.js
@@ -0,0 +1,6 @@
+﻿$("button").click(function () {
+    $.post('https://edu.santhawa.lk/apiv1/citizen', { email: 'thi@jkl.lk' }, function (response) {
+        console.log(response);
+        alert("success");
+    });
+});
